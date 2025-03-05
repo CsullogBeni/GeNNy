14b4c39e32a65b13b18985a03453baea48aed58b
Csüllög Benedek Szabolcs <bvdorj@inf.elte.hu>
2025-03-05 19:28:29 +0100

    model init

---
+++ GrapLearner/model.py
@@ -0,0 +1,8 @@
+from GrapLearner.p4_graph import P4Graph
+from GrapLearner.trainer import Trainer
+
+
+class Model:
+    def __init__(self):
+        self._graphs = []
+        self._trainer = Trainer()
