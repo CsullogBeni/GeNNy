13843b6b96dfd00813415071b590d5ccaa3804ee
Csüllög Benedek Szabolcs <bvdorj@inf.elte.hu>
2025-03-19 19:22:24 +0100

    Similar to trainer.py, works, before change

--- models/assignment_model.py
+++ models/assignment_model.py
@@ -5,12 +5,14 @@ import torch.nn as nn
 import torch.optim as optim
 from torch_geometric.data import Data
 from sklearn.preprocessing import OneHotEncoder
+import torch.nn.functional as F
 
 from models.gnn import GNN
+from graph.p4_graph import P4Graph
 from pretty_printer.pretty_printer import PrettyPrinter
 
 
-class Trainer:
+class AssignmentTrainer:
     def __init__(self):
         self._classes = None
         self._values = None
@@ -29,7 +31,6 @@ class Trainer:
         self._values = list(set(filter(None, nx.get_node_attributes(g, 'value').values())))
 
         self._class_encoder.fit(np.array(self._classes).reshape(-1, 1))
-
         self._value_encoder.fit(np.array(self._values).reshape(-1, 1))
         self._node_vectors = {node: self._node_to_vector(g.nodes[node]) for node in g.nodes}
 
@@ -47,7 +48,7 @@ class Trainer:
         self._optimizer = optim.Adam(self._model.parameters(), lr=0.001)
 
         original_data = self._data
-        number_of_training_epochs = 20000
+        number_of_training_epochs = 10000
         for epoch in range(number_of_training_epochs):
             if epoch % 100 == 0 and epoch > 0:
                 self._data = self._modify_graph(original_data, removal_ratio=(epoch / number_of_training_epochs) * 0.5)
@@ -57,6 +58,37 @@ class Trainer:
 
             if epoch == 200:
                 self._data = self._modify_graph(self._data, removal_ratio=0.2)
+
+        self._reconstructed_data = self._reconstruct_graph(original_data)
+
+    @property
+    def get_reconstructed_graph(self) -> nx.DiGraph:
+        g = nx.DiGraph()
+        g.add_edges_from(self._reconstructed_data.edge_index.T.tolist())
+
+        node_attrs = {}
+        for node in g.nodes:
+            vector = self._reconstructed_data.x[node].numpy().tolist()
+
+            value_encoded_size = len(self._value_encoder.categories_[0])
+            value_vector = vector[4:4 + value_encoded_size]
+            class_vector = vector[4 + value_encoded_size:]
+
+            class_decoded = self._class_encoder.categories_[0][np.argmax(class_vector)]
+            value_decoded = (self._value_encoder.categories_[0][np.argmax(value_vector)]
+                             if np.max(value_vector) > 0.2 and class_decoded == "TerminalNodeImpl" else None)
+
+            node_attrs[node] = {
+                "nodeId": int(node),  # int(round(vector[0])),
+                "line": int(round(vector[1])),
+                "start": int(round(vector[2])),
+                "end": int(round(vector[3])),
+                "value": value_decoded,
+                "class_": class_decoded
+            }
+
+        nx.set_node_attributes(g, node_attrs)
+        return g
 
     def _node_to_vector(self, attrs):
         node_id = attrs.get("nodeId", 0) / 10
@@ -86,99 +118,45 @@ class Trainer:
         return loss.item()
 
     @staticmethod
-    def _modify_graph(data, removal_ratio=0.2):
+    def _modify_graph(data, removal_ratio=0.1):  # Csökkentsd az eltávolítási arányt
         num_edges = data.edge_index.shape[1]
         num_remove = int(removal_ratio * num_edges)
         mask = torch.ones(num_edges, dtype=torch.bool)
-
-        # Legnagyobb indexű node-hoz kapcsolódó élek eltávolítása
         nodes = data.edge_index[0].unique()
         num_nodes = nodes.shape[0]
-
-        # Ha túl nagy a num_remove, csökkentsük le
         num_remove = min(num_remove, num_nodes)
-
         if num_remove > 0:
             nodes_to_remove = nodes.topk(num_remove).values
-            remove_indices = [i for i, (src, _) in enumerate(data.edge_index.T.tolist())
-                              if src in nodes_to_remove]
+            remove_indices = [i for i, (src, _) in enumerate(data.edge_index.T.tolist()) if src in nodes_to_remove]
             mask[remove_indices] = False
-
         new_edge_index = data.edge_index[:, mask]
         return Data(x=data.x, edge_index=new_edge_index)
 
     @staticmethod
     def loss_fn(output, target, edge_index):
         node_loss = nn.MSELoss()(output[:, :4] * 10, target[:, :4] * 100)  # Nagyobb súlyt adunk az első 4 értéknek
-        feature_loss = nn.MSELoss()(output[:, 4:], target[:, 4:])  # Az egyéb jellemzők normál súlyon maradnak
-
+        feature_loss = nn.MSELoss()(output[:, 4:], target[:, 4:])
         pred_edges = torch.mm(output, output.T)
         edge_target = torch.zeros_like(pred_edges)
         edge_target[edge_index[0], edge_index[1]] = 1
         edge_loss = nn.BCEWithLogitsLoss()(pred_edges, edge_target)
-
         return node_loss + feature_loss + edge_loss
 
-    def generate(self, left_attr: str, right_attr: str, num_nodes=10):
-        """Teljesen új gráfot generál a tanult mintázatok alapján, csak a modell és az encoderek használatával."""
-
+    def _reconstruct_graph(self, data):
         self._model.eval()
         with torch.no_grad():
-            # Véletlenszerű bemenet generálása
-            input_noise = torch.randn((num_nodes, self._model.encoder.in_channels))
+            reconstructed_x = self._model(data)
+        return Data(x=reconstructed_x, edge_index=data.edge_index)
 
-            # Modell futtatása
-            encoded = self._model.encoder(input_noise, torch.empty((2, 0), dtype=torch.long))
-            generated_output = self._model.decoder(encoded, torch.empty((2, 0), dtype=torch.long))
-
-        # Új gráf létrehozása
-        new_graph = nx.DiGraph()
-
-        # Oszlopok számának ellenőrzése
-        class_encoding_size = len(self._class_encoder.categories_[0])  # Ennyi oszlopot vár az encoder
-        value_encoding_size = len(self._value_encoder.categories_[0]) if self._value_encoder.categories_ else 0
-
-        for i, node_data in enumerate(generated_output):
-            # A megfelelő szeletelés biztosítása
-            class_vector = node_data[-class_encoding_size:].reshape(1, -1)
-            value_vector = node_data[4: 4 + value_encoding_size].reshape(1, -1) if value_encoding_size > 0 else None
-
-            # Osztály dekódolása
-            class_decoded = self._class_encoder.inverse_transform(class_vector)[0][0]
-
-            # Érték dekódolása
-            value_decoded = None
-            if class_decoded == "TerminalNodeImpl":
-                if value_vector is not None:
-                    value_decoded = self._value_encoder.inverse_transform(value_vector)[0][0]
-                value_decoded = left_attr if i == 0 else right_attr
-
-            new_graph.add_node(i, class_=class_decoded, value=value_decoded)
-
-        # **Élek generálása**
-        edge_probabilities = torch.mm(generated_output, generated_output.T)
-        edge_threshold = 0.5
-        for i in range(num_nodes):
-            for j in range(num_nodes):
-                if edge_probabilities[i, j] > edge_threshold:
-                    new_graph.add_edge(i, j)
-
-        return new_graph
-
-
-from graph.p4_graph import P4Graph
 
 og_graph = P4Graph()
-og_graph.read_from_json(
-    r"C:\Users\Acer\OneDrive - Eotvos Lorand Tudomanyegyetem\Dokumentumok\git\P4\GrapLearner\full_graphs\assignment.json")
-assignment_model = Trainer()
-assignment_model.train(og_graph.get_graph)
-left_attr = "hdr.ethernet.dstAddr"
-right_attr = "dstAddr"
-reconstructed_graph = assignment_model.generate(left_attr, right_attr)
-print(reconstructed_graph)
+og_graph.read_from_json(r"assignment.json")
+expression_context_model = AssignmentTrainer()
+expression_context_model.train(og_graph.get_graph)
+terminal_node_impl = "dstAddr"
+reconstructed_graph = expression_context_model.get_reconstructed_graph
 
-'''differences = og_graph.compare_graphs(reconstructed_graph)
+differences = og_graph.compare_graphs(reconstructed_graph)
 print(differences['missing_edges'])
 print(differences['extra_edges'])
 for item in differences['node_attribute_differences']:
@@ -186,4 +164,4 @@ for item in differences['node_attribute_differences']:
     print('Original:', item['original'])
     print('Reconstructed:', item['reconstructed'])
 print(PrettyPrinter(og_graph.get_graph).get_script)
-print(PrettyPrinter(reconstructed_graph).get_script)'''
+print(PrettyPrinter(reconstructed_graph).get_script)
