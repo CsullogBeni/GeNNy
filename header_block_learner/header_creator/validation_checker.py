ec1391c8a28cc5deafbbb6ac5f906f54ee6eef06
Csüllög Benedek Szabolcs <bvdorj@inf.elte.hu>
2025-09-12 10:54:17 +0200

    validation checker saves the script and visualize the graph

--- header_block_learner/header_creator/validation_checker.py
+++ header_block_learner/header_creator/validation_checker.py
@@ -1,13 +1,19 @@
 import os
 
 from graph.graph_builder import GraphBuilder
+from graph.graph_visualizer import GraphVisualizer
+
 from pretty_printer.pretty_printer import PrettyPrinter
 
-
-val_data = "validation"
+val_data = "validation_out"
 
 for file in os.listdir(val_data):
     graph_builder = GraphBuilder()
     graph_builder.load_data(os.path.join(val_data, file))
     pretty_printer = PrettyPrinter(graph_builder.graph)
     print(file + ':\n' + pretty_printer.get_script)
+    out_path = os.path.join("generated_p4_files", file[:-5] + '.p4')
+    pretty_printer.save_script(out_path)
+    print('Saved to: ' + out_path)
+    graph_visualizer = GraphVisualizer()
+    graph_visualizer.draw(graph_builder.graph)
